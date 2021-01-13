25c9cfdb1dc5d00d6f731628a96295d48c68162b
Shraddha13Jain <shraddhaj829@gmail.com>
2021-01-13 16:55:45 +0530

    new

--- app.js
+++ app.js
@@ -21,15 +21,18 @@ mongoose.connect(process.env.MONGO_URL,{
             console.log(err);
             process.exit();
         }) 
-
-
 app.get('/',(req,res)=>{
     res.send("welcome to blog app");
 });
 
+
+app.use('*',(req,res,next)=>{
+ res.status(404).json({"msg":"NOT FOUND"});
+});
+
 require('./routes/route')(app);
 
-const port=3000;
+const port=process.env.PORT || 3000;
 app.listen(port,(err)=>{
     if(err) console.log(err);
     else console.log(`post is live at ${port}`);
