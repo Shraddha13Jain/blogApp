60e71c3ddc7fc03e4e8a9fa12cc76b724181f5f1
Shraddha13Jain <shraddhaj829@gmail.com>
2021-01-13 18:04:40 +0530

    newc

--- app.js
+++ app.js
@@ -25,12 +25,12 @@ app.get('/',(req,res)=>{
     res.send("welcome to blog app");
 });
 
+require('./routes/route')(app);
 
 app.use('*',(req,res,next)=>{
  res.status(404).json({"msg":"NOT FOUND"});
 });
 
-require('./routes/route')(app);
 
 const port=process.env.PORT || 3000;
 app.listen(port,(err)=>{
